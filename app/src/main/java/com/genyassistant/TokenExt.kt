141da72f29baa8f75f474402490d66580dcea57b
CarsaiMz <carsaimozambique@gmail.com>
2026-06-05 05:41:57 +0200

    Atualizar tokens e SandboxID

--- app/src/main/java/com/genyassistant/TokenExt.kt
+++ app/src/main/java/com/genyassistant/TokenExt.kt
@@ -1,10 +1,10 @@
 package com.genyassistant
 
 // TODO: Add your Sandbox ID here
-const val sandboxID = ""
+const val sandboxID = "genyassistant-2dsou2"
 
 // NOTE: If you prefer not to use the token server for testing, you can generate your
 // tokens manually by visiting https://cloud.livekit.io/projects/p_/settings/keys
 // and using one of your API Keys to generate a token with custom TTL and permissions.
-const val hardcodedUrl = ""
-const val hardcodedToken = ""
+const val hardcodedUrl = "wss://geny-assistant-47cqg7ug.livekit.cloud"
+const val hardcodedToken = APIuxDyCtS2Swy5"
